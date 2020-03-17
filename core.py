f4489a6c132dd666d56cafd1f0a115197822df0d
Anthony Byuraev <anthony.byuraev@gmail.com>
2020-03-17 22:43:20 +0300

    Upgrade LICENCE, lint core.py

--- core.py
+++ core.py
@@ -1,28 +1,29 @@
 ﻿"""
-    core.py -- модуль, в котором описан класс 'EulerianGrid' для решения ОЗВБ
-        в газодинамической постановке
+core.py -- модуль, в котором описан класс 'EulerianGrid' для решения ОЗВБ
+    в газодинамической постановке
 
-    Author: Anthony Byuraev 
-    Email: toha.byuraev@gmail.com
+Author: Anthony Byuraev 
+Email: anthony.byuraev@gmail.com
 """
 
 __author__ = 'Anthony Byuraev'
+
 
 import numpy as np
 
 
 class EulerianGrid(object):
     """
-        Класс, содержащий решение основной задачи внутренней баллистики (пневматика) 
-            в газодинамической постановке на подвижной сетке по методу Эйлера
+    Класс, содержащий решение основной задачи внутренней баллистики
+        (пневматика) в газодинамической постановке на подвижной сетке по
+            методу Эйлера
 
-        Parameters:
-            init_data {dict} -- словарь с начальными данными хар-к АО
+    Parameters:
+        init_data {dict} -- словарь с начальными данными хар-к АО
 
-        Keyword arguments:
-            pressInit {float} -- начальное давление в каморе
-            L0 {float} -- начальная длина запоршневого пространства
-
+    Keyword arguments:
+        pressInit {float} -- начальное давление в каморе
+        L0 {float} -- начальная длина запоршневого пространства
     """
 
     def __str__(self):
@@ -44,27 +45,30 @@ class EulerianGrid(object):
 
     def __init__(self, init_data, **kwargrs):
         self.num_coor = init_data.get('numCoor', self.defaultChar['numCoor'])
-        self.press = init_data.get('pressInit', \
-            kwargrs.get('pressInit', self.defaultChar['pressInit']))
+        self.press = init_data.get(
+            'pressInit', kwargrs.get(
+                'pressInit', self.defaultChar['pressInit']))
         self.ro = init_data.get('ro', self.defaultChar['ro'])
         self.kurant = init_data.get('Ku', self.defaultChar['Ku'])
-        self.shellMass = init_data.get('shellMass', self.defaultChar['shellMass'])
+        self.shellMass = init_data.get(
+            'shellMass', self.defaultChar['shellMass'])
         self.tau = 0
         # длина ячейки на пред. шаге (коор-та 1 границы)
         #   необходимо для расчета веторов q
         self.x_prev = 0
         self.S = np.pi * init_data['d'] ** 2 / 4
-        self.L0 = init_data.get('L0', \
-            kwargrs.get('L0', self.defaultChar['L0']))
-        self.L = init_data.get('L', \
-            kwargrs.get('L', self.defaultChar['L']))
+        self.L0 = init_data.get(
+            'L0', kwargrs.get('L0', self.defaultChar['L0']))
+        self.L = init_data.get('L', kwargrs.get('L', self.defaultChar['L']))
 
         # параметры газа
         self.R = init_data.get('R', self.defaultChar['R'])
         self.k = init_data.get('k', self.defaultChar['k'])
 
-        self.energy_cell = np.full(self.num_coor, self.press / (self.k - 1) / self.ro)
-        self.c_cell = np.full(self.num_coor, np.sqrt(self.k * self.press / self.ro))
+        self.energy_cell = np.full(
+            self.num_coor, self.press / (self.k - 1) / self.ro)
+        self.c_cell = np.full(
+            self.num_coor, np.sqrt(self.k * self.press / self.ro))
         self.ro_cell = np.full(self.num_coor, self.ro)
         self.v_cell = np.zeros(self.num_coor)
         self.press_cell = np.zeros(self.num_coor)
@@ -75,12 +79,12 @@ class EulerianGrid(object):
 
         # Для расчета потока f (Векторы Ф)
         self.F_param_p = np.array([np.zeros(self.num_coor - 1),
-                                   np.zeros(self.num_coor - 1), 
+                                   np.zeros(self.num_coor - 1),
                                    np.zeros(self.num_coor - 1)])
         self.F_param_m = np.array([np.zeros(self.num_coor - 1),
-                                   np.zeros(self.num_coor - 1), 
+                                   np.zeros(self.num_coor - 1),
                                    np.zeros(self.num_coor - 1)])
-        
+
         self.c_interface = np.zeros(self.num_coor - 1)
         self.mah_interface = np.zeros(self.num_coor - 1)
         self.press_interface = np.zeros(self.num_coor - 1)
@@ -88,12 +92,12 @@ class EulerianGrid(object):
         self.x_interface = np.zeros(self.num_coor - 1)
 
         self.f_param = np.array([np.zeros(self.num_coor - 1),
-                                 self.press_cell[1:], 
+                                 self.press_cell[1:],
                                  np.zeros(self.num_coor - 1)])
-        self.q_param = np.array([self.ro_cell, 
+        self.q_param = np.array([self.ro_cell,
                                  self.ro_cell * self.v_cell,
                                  self.ro_cell * (self.energy_cell + self.v_cell ** 2 / 2)])
-        
+
     def _get_q(self):
         coef_stretch = self.x_prev / self.x_interface[1]
         self.q_param[0][1:-1] = coef_stretch * (self.q_param[0][1:-1] - self.tau / self.x_prev * \
@@ -110,25 +114,25 @@ class EulerianGrid(object):
         self._border()
 
     def _get_f(self):
-        self.f_param[0] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[0] + \
+        self.f_param[0] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[0] +
             self.F_param_m[0]) - abs(self.mah_interface) * (self.F_param_p[0] - self.F_param_m[0]))
-        self.f_param[1] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[1] + \
+        self.f_param[1] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[1] +
             self.F_param_m[1]) - abs(self.mah_interface) * (self.F_param_p[1] - self.F_param_m[1])) + \
                 self.press_interface
-        self.f_param[2] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[2] + \
+        self.f_param[2] = self.c_interface / 2 * (self.mah_interface * (self.F_param_p[2] +
             self.F_param_m[2]) - abs(self.mah_interface) * (self.F_param_p[2] - self.F_param_m[2])) + \
                 self.press_interface * self.v_interface
 
     def _get_F_mines(self):
         self.F_param_m[0] = self.ro_cell[:-1]
         self.F_param_m[1] = self.ro_cell[:-1] * self.v_cell[:-1]
-        self.F_param_m[2] = self.ro_cell[:-1] * (self.energy_cell[:-1] + \
+        self.F_param_m[2] = self.ro_cell[:-1] * (self.energy_cell[:-1] +
             self.v_cell[:-1] ** 2 / 2 + self.press_cell[:-1] / self.ro_cell[:-1])
 
     def _get_F_plus(self):
         self.F_param_p[0] = self.ro_cell[1:]
         self.F_param_p[1] = self.ro_cell[1:] * self.v_cell[1:]
-        self.F_param_p[2] = self.ro_cell[1:] * (self.energy_cell[1:] + \
+        self.F_param_p[2] = self.ro_cell[1:] * (self.energy_cell[1:] +
             self.v_cell[1:] ** 2 / 2 + self.press_cell[1:] / self.ro_cell[1:])
 
     def _get_c_interface(self):
@@ -137,7 +141,7 @@ class EulerianGrid(object):
     def _get_mah_mp(self):
         self.mah_cell_m = (self.v_cell[:-1] - self.v_interface) / self.c_interface
         self.mah_cell_p = (self.v_cell[1:] - self.v_interface) / self.c_interface
-    
+
     def _get_mah_press_interface(self):
         self.mah_interface = self.fetta_plus() + self.fetta_mines()
         self.press_interface = self.getta_plus() * self.press_cell[:-1] + \
@@ -154,7 +158,7 @@ class EulerianGrid(object):
 
         self.v_cell[0] = -self.v_cell[1]
         self.q_param[1][0] = self.ro_cell[0] * self.v_cell[0]
-        self.q_param[1][self.num_coor - 1] = self.q_param[0][self.num_coor - 1] * \
+        self.q_param[1][self.num_coor - 1] = self.q_param[0][self.num_coor - 1] *
             (2 * self.v_interface[self.num_coor - 2] - self.v_cell[self.num_coor - 2])
 
         self.q_param[2][0] = self.q_param[2][1]
@@ -185,8 +189,10 @@ class EulerianGrid(object):
         # need to fix with vector calculations
         buf = []
         for mah in self.mah_cell_m:
-            if abs(mah) >= 1: buf.append(0.5 * (mah + abs(mah)))
-            else: buf.append(0.25 * (mah + 1) ** 2 * (1 + 4 * 1 / 8 * (mah - 1) ** 2))
+            if abs(mah) >= 1:
+                buf.append(0.5 * (mah + abs(mah)))
+            else:
+                buf.append(0.25 * (mah + 1) ** 2 * (1 + 4 * 1 / 8 * (mah - 1) ** 2))
         return np.asarray(buf)
 
     def fetta_mines(self):
@@ -196,8 +202,10 @@ class EulerianGrid(object):
         # need to fix with vector calculations
         buf = []
         for mah in self.mah_cell_p:
-            if abs(mah) >= 1: buf.append(0.5 * (mah - abs(mah)))
-            else: buf.append(- 0.25 * (mah - 1) ** 2 * (1 + 4 * 1 / 8 * (mah + 1) ** 2))
+            if abs(mah) >= 1:
+                buf.append(0.5 * (mah - abs(mah)))
+            else:
+                buf.append(- 0.25 * (mah - 1) ** 2 * (1 + 4 * 1 / 8 * (mah + 1) ** 2))
         return np.asarray(buf)
 
     def getta_plus(self):
@@ -206,8 +214,10 @@ class EulerianGrid(object):
         """
         buf = []
         for mah in self.mah_cell_m:
-            if abs(mah) >= 1: buf.append((mah + abs(mah)) / 2 / mah)
-            else: buf.append((mah + 1) ** 2 * ((2 - mah) / 4 + 3 / 16 * mah * (mah - 1) ** 2))
+            if abs(mah) >= 1:
+                buf.append((mah + abs(mah)) / 2 / mah)
+            else:
+                buf.append((mah + 1) ** 2 * ((2 - mah) / 4 + 3 / 16 * mah * (mah - 1) ** 2))
         return np.asarray(buf)
 
     def getta_mines(self):
@@ -216,15 +226,17 @@ class EulerianGrid(object):
         """
         buf = []
         for mah in self.mah_cell_p:
-            if abs(mah) >= 1: buf.append((mah - abs(mah)) / 2 / mah)
-            else: buf.append((mah - 1) ** 2 * ((2 + mah) / 4 - 3 / 16 * mah * (mah + 1) ** 2))
+            if abs(mah) >= 1:
+                buf.append((mah - abs(mah)) / 2 / mah)
+            else:
+                buf.append((mah - 1) ** 2 * ((2 + mah) / 4 - 3 / 16 * mah * (mah + 1) ** 2))
         return np.asarray(buf)
 
     def run(self):
         """
             Метод - решение. Последовательное интегрирование параметров системы.
         """
-        
+
         # Вычисление координат границ в начальный момент времени
         self._new_x_interf(self.L0)
 
@@ -242,7 +254,7 @@ class EulerianGrid(object):
             self._new_x_interf(self._end_vel_x()[1])
             self.v_interface[-1] = self._end_vel_x()[0]
             # линейное распределение скорости
-            self.v_interface = ( self.v_interface[-1] / self.x_interface[-1] ) * self.x_interface
+            self.v_interface = (self.v_interface[-1] / self.x_interface[-1]) * self.x_interface
 
             # заполнение массивов для графиков
             if len(self.time) == 0:
@@ -266,5 +278,6 @@ class EulerianGrid(object):
             if self.x_interface[-1] >= self.L:
                 break
 
+
 if __name__ == '__main__':
-    print('Error: попытка запустить coreP.py')
+    print('Error: попытка запустить core.py')
